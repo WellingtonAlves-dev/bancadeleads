6e93b13ff313293ae787d3b483461d3156d5335c
Wellington Alves <well.alves090@gmail.com>
2024-10-19 03:58:45 -0300

    Leads Controller

--- resources/views/pages/lead_search.blade.php
+++ resources/views/pages/lead_search.blade.php
@@ -1,10 +1,10 @@
 <input type="hidden" id="totalLeads" value="{{$totalLeads}}"/>
 <input type="hidden" id="totalLeadsCarregada" value="{{count($leads)}}"/>
 @if(count($leads) == 0)
-    <h1 style="text-align: center">Infelizmente no momento as leads para este DDD
+    <h4 style="text-align: center; margin-top: 25px;">Infelizmente no momento as leads para este DDD
         já foram todas vendidas, fique atento que em breve entrão mais leads no horário
         de funcionamento da plataforma
-    </h1>
+    </h4>
 @endif
 @foreach($leads as $lead)
 @if($lead->disponivel)
