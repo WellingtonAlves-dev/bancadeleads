e07f7748a3a18eb7dda18a0f430990505cab3403
Wellington Alves <well.alves090@gmail.com>
2024-10-19 04:02:11 -0300

    Total Leads

--- resources/views/pages/lead_search.blade.php
+++ resources/views/pages/lead_search.blade.php
@@ -1,15 +1,23 @@
 <input type="hidden" id="totalLeads" value="{{$totalLeads}}"/>
 <input type="hidden" id="totalLeadsCarregada" value="{{count($leads)}}"/>
-@if(count($leads) == 0)
-    <h4 style="text-align: center; margin-top: 25px;">Infelizmente no momento as leads para este DDD
-        já foram todas vendidas, fique atento que em breve entrão mais leads no horário
-        de funcionamento da plataforma
-    </h4>
-@endif
+
+@php
+$totalLeadsDisponivel = 0;
+@endphp
 @foreach($leads as $lead)
 @if($lead->disponivel)
     <div class="col-12 justify-content-center">
         @include("components.lead")
     </div>
+    @php
+        $totalLeadsDisponivel += 1;
+    @endphp
 @endif
 @endforeach
+
+@if($totalLeadsDisponivel == 0)
+    <h4 style="text-align: center; margin-top: 25px;">Infelizmente no momento as leads para este DDD
+        já foram todas vendidas, fique atento que em breve entrão mais leads no horário
+        de funcionamento da plataforma
+    </h4>
+@endif
