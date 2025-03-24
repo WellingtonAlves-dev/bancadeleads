1e7d790a19e7c370f745363245b6734c4e9eb08a
Wellington R Alves <well.alves090@gmail.com>
2025-03-23 22:12:49 -0300

    Bana

--- resources/views/mail/recovery.blade.php
+++ resources/views/mail/recovery.blade.php
@@ -114,8 +114,8 @@
         <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
           <tr>
             <td align="center" valign="top" style="padding: 36px 24px;">
-              <a href="https://www.indicasaude.com.br/" target="_blank" style="display: inline-block;">
-                <img src="https://www.indicasaude.com.br/images/logo.png" alt="Logo" border="0" width="120" style="display: block; width: 120px; max-width: 120px; min-width: 120px;">
+              <a href="https://www.bancadeleads.com.br/" target="_blank" style="display: inline-block;">
+                <img src="https://bancadeleads.com.br/assets/img/logo.png" alt="Logo" border="0" width="48" style="display: block; width: 48px; max-width: 48px; min-width: 48px;">
               </a>
             </td>
           </tr>
