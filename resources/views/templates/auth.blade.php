e5f631132b505abf39f07c8ab008ed969b856def
Well <well.alves090@gmail.com>
2025-03-25 20:11:08 -0300

    favicon

--- resources/views/templates/auth.blade.php
+++ resources/views/templates/auth.blade.php
@@ -19,6 +19,8 @@
 
     <!-- Custom styles for this template-->
     <link href="{{asset("assets")}}/css/sb-admin-2.css" rel="stylesheet">
+    <link rel="icon" href="{{url("favicon.ico")}}" type="image/x-icon"/>
+
 
 </head>
 <body class="bg-gradient-primary">
